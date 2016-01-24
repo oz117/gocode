db1a8fa05e3a41aaf9835be1af237e9882b317c1
Andre Paulos <andre.paulos@epitech.eu>
2016-01-24 16:04:37 +0100

    Used go fmt to clean code on reverse_hello.go

--- reverse_hello/reverse_hello.go
+++ reverse_hello/reverse_hello.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "github.com/oz117/training/stringutil"
+	"fmt"
+	"github.com/oz117/training/stringutil"
 )
 
 func main() {
-    fmt.Printf(stringutil.Reverse("Hello world!") + "\n")
+	fmt.Printf(stringutil.Reverse("Hello world!") + "\n")
 }
