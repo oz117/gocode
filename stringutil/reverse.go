ed8d705f5527a543df5bea08a8acbec5e1a69918
paulos_a <paulos_a@epitech.eu>
2016-01-24 15:35:53 +0100

    Added reverse string lib and to see what it in action

---
+++ stringutil/reverse.go
@@ -0,0 +1,10 @@
+package stringutil
+
+func Reverse(s string) string {
+    ret := []rune(s)
+
+    for i, j := 0, len(ret) - 1; i < len(ret) / 2; i, j = i + 1, j - 1 {
+        ret[i], ret[j] = ret[j], ret[i]
+    }
+    return string(ret)
+}
