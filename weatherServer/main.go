cbf05eba390e4a738a54b8b77b3447eb7c25471b
paulos_a <paulos_a@epitech.eu>
2016-02-10 12:05:30 +0100

    It is now a concurrent weather service meaning that each api will be called at the same time using go s concurrency primitives

--- weatherServer/main.go
+++ weatherServer/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,15 +9,14 @@ import (
 )
 
 var addr = flag.String("localhost", ":4000", "http service address") // http server address
-var templ = template.Must(template.New("weather").Parse(TemplateStr))
 
 /*
  * Retrieves the weatherData structure and shows the html page
  */
 func getWeather(w http.ResponseWriter, r *http.Request) {
 	mw := multiWeatherProvider{
-		openWeatherMap{apikey: "your api key"},
-		weatherUnderground{apikey: "your api key"},
+		openWeatherMap{apikey: "88ed45090cdc91cf5202c4217e8fabd9"},
+		weatherUnderground{apikey: "fe15c5aa02cd1a16"},
 	}
 	city := strings.SplitN(r.URL.Path, "/", 3)[2]
 
@@ -27,8 +25,6 @@ func getWeather(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	templ.Execute(w, r.FormValue("s"))
-
 	temp := strconv.FormatFloat(data, 'E', 2, 64)
 	w.Write([]byte("<div>" + temp + "</div>"))
 }
@@ -37,7 +33,6 @@ func getWeather(w http.ResponseWriter, r *http.Request) {
  * Writes a simple welcome message
  */
 func welcome(w http.ResponseWriter, r *http.Request) {
-	templ.Execute(w, r.FormValue("s"))
 	w.Write([]byte("<div>Welcome to my weather provider</div>"))
 }
 
@@ -47,4 +42,5 @@ func main() {
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	log.Printf("Server started on port 8000")
 }
--- weatherServer/temperature.go
+++ weatherServer/temperature.go
@@ -33,51 +33,11 @@ type weatherUnderground struct {
 type multiWeatherProvider []weatherProvider
 
 /*
- * Calls the temperature function of each weather provider
- * Returns the average temperature
- */
-func (providers multiWeatherProvider) temperature(city string) (float64, error) {
-	sum := 0.0
-
-	for _, provider := range providers {
-		k, err := provider.temperature(city)
-		if err != nil {
-			return 0, err
-		}
-		sum += k
-	}
-	return sum / float64(len(providers)), nil
-}
-
-/*
- * Retrieves the temperature from openweathermap.org
- * and returns it in celsius
- */
-func (w openWeatherMap) temperature(city string) (float64, error) {
-	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + w.apikey)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-	var d struct {
-		Main struct {
-			Kelvin float64 `json:"temp"`
-		} `json:"main"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return 0, err
-	}
-	celsius := d.Main.Kelvin - 273.15
-	log.Printf("openWeatherMap: %s: %.2f", city, celsius)
-	return celsius, nil
-}
-
-/*
  * Retrieves the temperature from wunderground.com
  * and returns it in celsius
  */
 func (w weatherUnderground) temperature(city string) (float64, error) {
-	resp, err := http.Get("http://api.wunderground.com/api/" + w.apikey + "/conditions/q/" + city + ".json")
+	resp, err := http.Get("http://api.wunderground.com/api/" + w.apikey + "/conditions/q/FR/" + city + ".json")
 	if err != nil {
 		return 0, err
 	}
@@ -92,6 +52,63 @@ func (w weatherUnderground) temperature(city string) (float64, error) {
 	}
 	log.Printf("Weather Underground: %s: %2.f", city, d.Observation.Celsius)
 	return d.Observation.Celsius, nil
+}
+
+/*
+ * Retrieves the temperature from openweathermap.org
+ * and returns it in celsius
+ */
+func (w openWeatherMap) temperature(city string) (float64, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + w.apikey)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	var d struct {
+		Weather []struct {
+			Description string `json:"description"`
+		} `json:"weather"`
+		Main struct {
+			Kelvin float64 `json:"temp"`
+		} `json:"main"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+	log.Printf("Condition: %s", d.Weather[0].Description)
+	celsius := d.Main.Kelvin - 273.15
+	log.Printf("openWeatherMap: %s: %.2f", city, celsius)
+	return celsius, nil
+}
+
+/*
+ * Calls the temperature function of each weather provider
+ * Returns the average temperature
+ */
+func (providers multiWeatherProvider) temperature(city string) (float64, error) {
+	temps := make(chan float64, len(providers))
+	errs := make(chan error, len(providers))
+	sum := 0.0
+
+	for _, provider := range providers {
+		go func(p weatherProvider) {
+			k, err := p.temperature(city)
+			if err != nil {
+				errs <- err
+				return
+			}
+			temps <- k
+		}(provider)
+	}
+	for i := 0; i < len(providers); i++ {
+		select {
+		case temp := <-temps:
+			sum += temp
+		case err := <-errs:
+			return 0, err
+		}
+	}
+	return sum / float64(len(providers)), nil
 }
 
 /*
