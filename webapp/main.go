dfef81d3d3817b75c36adf37ca40822c4f686bee
paulos_a <paulos_a@epitech.eu>
2016-02-11 16:23:48 +0100

    The path in the URL is now checked to avoid security flaws

--- webapp/main.go
+++ webapp/main.go
@@ -1,37 +1,43 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
-type Page struct {
+var templates = template.Must(template.ParseFiles("Views/view.html", "Views/edit.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+type page struct {
 	Title string
 	Body  []byte
 } // Content of a page
 
-var templates = template.Must(template.ParseFiles("Views/view.html", "Views/edit.html"))
-
-func (p *Page) savePage() error {
+func (p *page) savepage() error {
 	fileName := p.Title + ".txt"
 	return ioutil.WriteFile(fileName, p.Body, 0600)
 }
 
-func loadPage(title string) (*Page, error) {
+func loadpage(title string) (*page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	return &page{Title: title, Body: body}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
 
-	p, err := loadPage(title)
+	if err != nil {
+		return
+	}
+	p, err := loadpage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
@@ -40,31 +46,46 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
 
-	p, err := loadPage(title)
 	if err != nil {
-		p = &Page{Title: title}
+		return
+	}
+	p, err := loadpage(title)
+	if err != nil {
+		p = &page{Title: title}
 	}
 	renderTemplate(w, "edit.html", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("content")
 
-	p := &Page{Title: title, Body: []byte(body)}
-	if err := p.savePage(); err != nil {
+	p := &page{Title: title, Body: []byte(body)}
+	if err := p.savepage(); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+func renderTemplate(w http.ResponseWriter, tmpl string, p *page) {
 	if err := templates.ExecuteTemplate(w, tmpl, p); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
 }
 
 func main() {
